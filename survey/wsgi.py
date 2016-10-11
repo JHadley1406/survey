68155b55105be304ebeb87af2a9e7d9293fa3d71
Josiah Hadley <Josiah Hadley>
2016-10-11 16:52:33 -0700

    fix: Updated wsgi.py to match server deployment

--- survey/wsgi.py
+++ survey/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
-from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/home/public_html/survey/')
+sys.path.append('/home/public_html/survey/survey')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survey.settings")
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
